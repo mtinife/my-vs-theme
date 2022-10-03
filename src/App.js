741973ece49d4821ff18bcb59566d96fa9b352f0
Micah Tinife <micahking2k@gmail.com>
2022-10-02 21:23:28 -0400

    Added vs icons using react icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.sass'
 
+import { VscFiles } from "react-icons/vsc"
+
 function App() {
   return (
     <div className="App">
       <main className="vs-app">
-        <div className="Side-Bar"></div>
+        <div className="Side-Bar">
+          <VscFiles />
+        </div>
         <div className="Editor"></div>
         <div className="Terminal"></div>
         <div className="ActivityBar"></div>
